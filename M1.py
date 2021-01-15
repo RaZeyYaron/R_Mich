18d72479e4334dc35758c4fae9fdbda453898797
Yaron de Jongh <yaron@razey.nl>
2021-01-15 22:58:57 +0100

    Update M1.py
    Whitelines to clean up code

--- M1.py
+++ M1.py
@@ -1,8 +1,8 @@
 values = ['Roger Federer','Marat Safin','Roger Federer','Roger Federer','Novak Djokovic','Rafael Nadal','Roger Federer','Novak Djokovic','Novak Djokovic','Novak Djokovic','Stan Wawrinka','Novak Djokovic','Novak Djokovic']
 
-
 wins_federer = values.count('Roger Federer')
 wins_djokovic = values.count('Novak Djokovic')
+
 if wins_djokovic > wins_federer:
 	print("Djokovic has more wins")
 else:
